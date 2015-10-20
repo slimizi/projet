075400fbfbbad3eef56a06b5caff80508d1cf04f
slimizi <slim190@hotmail.fr>
2015-10-20 15:39:52 +0200

    save

--- T_declaration.c
+++ T_declaration.c
@@ -1,1 +1,28 @@
-T_declaration.c
+#include <stdio>
+#include <stdlib>
+#include "Table_Dec.h"
+
+/*	------------====DEFINITION DE LA STRUCTURE T_DECLARATION====------------
+
+typedef enum Nature Nature
+enum Nature
+{
+	T_BASE,T_STRUCT,T_TAB,T_VAR,T_PROC,T_FONC
+};
+
+struct cellule_dec
+{
+	Nature nature;
+	int suiv;
+	int region;
+	int descr;
+	// TYPE EXECUTION
+};
+
+typedef T_declaration *cellule_dec;
+
+*/
+
+// ------------==== Implementation des fonctions ====------------
+
+
--- T_declaration.h
+++ T_declaration.h
@@ -1,1 +1,33 @@
-T_declaration.h
+#include <stdio>
+#include <stdlib>
+
+#define DEBORD 300
+#define TAILLE 500
+
+
+typedef enum Nature Nature
+enum Nature
+{
+	T_BASE,T_STRUCT,T_TAB,T_VAR,T_PROC,T_FONC
+};
+
+struct cellule_dec
+{
+	Nature nature;
+	int suiv;
+	int region;
+	int descr;
+	// TYPE EXECUTION
+};
+
+typedef T_declaration *cellule_dec;
+
+
+//Fonctions de gestion de la région
+void init_region();
+void incr_region();
+
+
+
+void inserer(int indice,Nature n,int region, )
+
